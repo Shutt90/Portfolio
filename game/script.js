d7b34291ab4dec5d151d2f1e2669957c9d061eaa
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-03-10 21:29:46 +0000

    Added Countdown Game(Base)

--- game/script.js
+++ game/script.js
@@ -22,21 +22,38 @@
 //   // if total >= 100 console.log("helloWorld")
 // });
 
-const calcButton = document.querySelector(".calculate");
-const lilSelectAll = document.querySelectorAll(".lilnumber");
-const bigSelectAll = document.querySelectorAll(".bignumber");
 const bigNums = [100, 75, 50, 25, 10];
 const lilNums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-const randomBig = bigNums[Math.floor(Math.random() * bigNums.length)];
-const randomLittle = lilNums[Math.floor(Math.random() * lilNums.length)];
 
-document.addEventListener("click", function (e) {
+// const calculateBig = function (...bigNums) {
+
+// }
+
+document.querySelector(".calculate").addEventListener("click", function (e) {
   e.preventDefault();
-  bigSelectAll.textContent = randomBig;
-  lilSelectAll.textContent = randomLittle;
 
+  const randomBig0 = bigNums[Math.floor(Math.random() * bigNums.length)];
+  const randomBig1 = bigNums[Math.floor(Math.random() * bigNums.length)];
+  const randomLittle0 = lilNums[Math.floor(Math.random() * lilNums.length)];
+  const randomLittle1 = lilNums[Math.floor(Math.random() * lilNums.length)];
+  const randomLittle2 = lilNums[Math.floor(Math.random() * lilNums.length)];
+  const randomLittle3 = lilNums[Math.floor(Math.random() * lilNums.length)];
+  const lilArr = [randomLittle0, randomLittle1, randomLittle2, randomLittle3];
+  const bigArr = [randomBig0, randomBig1];
+  let final;
+
+  function total(min, max) {
+    return Math.trunc(Math.random() * (max - min) + min);
+  }
+
+  for (let i = 0; i < bigArr.length; i++) {
+    document.querySelector(`.bignumber${[i]}`).textContent = bigArr[i];
+  }
+  for (let i = 0; i < lilArr.length; i++) {
+    document.querySelector(`.lilnumber${[i]}`).textContent = lilArr[i];
+  }
+
+  document.querySelector(".final").textContent = total(100, 900);
   //Show in spans ".numbers" the big/lil nums they have to work with
   //Show the total they need to work out on the screen - needs html too
 });
-
-console.log(bigNums, lilNums, randomBig, randomLittle);
