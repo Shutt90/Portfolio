80a32615191849267c296bef06e0fe51b1d3e2b4
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-03-26 12:12:45 +0000

    Working UI
    Added the UI animations on the home page, need to get a drop down working on the hamburger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const slider = document.querySelector(".slider");
 const topLeft = document.getElementById("#top_left");
 const hamburger = document.querySelector(".hamburger");
 const myName = document.querySelector(".my_name");
+const lowerLinks = document.querySelector(".lower_links");
 
 const tl = new TimelineMax();
 
@@ -25,4 +26,11 @@ tl.fromTo(
     { x: "0%", ease: Power2.easeInOut },
     "-=1.2"
   )
-  .fromTo(hamburger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1");
+  .fromTo(hamburger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=1")
+  .fromTo(
+    lowerLinks,
+    1.2,
+    { opacity: 0 },
+    { opacity: 1, ease: Power2.easeInOut },
+    "-=0.6"
+  );
