3adda3ab393f19e29cb9ed538c0858676eda1927
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-03-28 13:35:32 +0100

    updated projects page to flip images of projects
    added more UI features, had an issue with creating a link from the new img src in the JS so opted to change the sidebar subtitles to link

--- UI.js
+++ UI.js
@@ -18,6 +18,19 @@
 // overlay.addEventListener("click", closePop);
 
 document.querySelector(".project01").addEventListener("click", function () {
-  document.querySelector(".main_img").src = "project01.png";
+  document.querySelector(".main_img").src = "FuzeCropped.png";
   document.querySelector(".my_name").classList.add("my_name_toggle");
+  document.querySelector(".main_head").classList.remove("main_head_toggle");
+});
+
+document.querySelector(".project02").addEventListener("click", function () {
+  document.querySelector(".main_img").src = "bccalc.png";
+  document.querySelector(".main_head").classList.add("main_head_toggle");
+  document.querySelector(".my_name").classList.remove("my_name_toggle");
+});
+
+document.querySelector(".project").addEventListener("click", function () {
+  document.querySelector(".main_img").src = "countdown.png";
+  document.querySelector(".main_head").classList.add("main_head_toggle");
+  document.querySelector(".my_name").classList.remove("my_name_toggle");
 });
