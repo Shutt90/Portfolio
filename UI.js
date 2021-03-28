90091a41af588668b5701e4ae35a9ec528e8ca71
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-03-28 13:19:31 +0100

    Changes to modal
    Put the modal in the main_img class to switch out, need to resize

--- UI.js
+++ UI.js
@@ -1,18 +1,23 @@
 "use strict";
 
-const projectEl = document.querySelector(".pop");
-const overlay = document.querySelector(".overlay");
-const projectKey = document.querySelector(".project01");
+// const projectEl = document.querySelector(".pop");
+// const overlay = document.querySelector(".overlay");
+// const projectKey = document.querySelector(".project01");
 
-const openPop = function () {
-  projectEl.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+// const openPop = function () {
+//   projectEl.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
 
-const closePop = function () {
-  projectEl.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+// const closePop = function () {
+//   projectEl.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
 
-projectKey.addEventListener("click", openPop);
-overlay.addEventListener("click", closePop);
+// projectKey.addEventListener("click", openPop);
+// overlay.addEventListener("click", closePop);
+
+document.querySelector(".project01").addEventListener("click", function () {
+  document.querySelector(".main_img").src = "project01.png";
+  document.querySelector(".my_name").classList.add("my_name_toggle");
+});
